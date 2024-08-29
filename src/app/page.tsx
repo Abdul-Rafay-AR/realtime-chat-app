27061333f2642cb659cce438fbc4908816126e41
Abdul-Rafay-AR <142048280+Abdul-Rafay-AR@users.noreply.github.com>
2024-08-29 15:36:54 +0500

    Configured @upstach/redis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-export default function Home() {
-    return (
-        <div className="text-red-500">Hello World!</div>
-    );
+import { db } from "@/lib/db";
+
+export default async function Home() {
+    await db.set("hello", "world");
+    return <div className="text-red-500">Hello World!</div>;
 }
